7adb1931b61ce2070b57b65b9682d2ab7876a06e
aceinetx <mouselin82@gmail.com>
2025-12-04 12:29:56 +0300

    Dont use isystem

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,6 @@
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = "build"}) -- generate compile commands
+set_policy("package.include_external_headers", false)
 
 set_policy("build.sanitizer.address", true) -- sanitizers
 set_policy("build.sanitizer.leak", true)
@@ -7,6 +8,7 @@ set_policy("build.sanitizer.leak", true)
 -- libs
 add_requires("raylib", {external=false})
 add_requires("rapidjson", {external=false})
+
 set_warnings("all") -- warns
 set_languages("c++17")
 
